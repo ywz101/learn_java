d6aa61a91145abf817720d717bdec071f31e755f
ywz101 <ywz101@gmail.com>
2024-01-29 23:08:28 +0800

    文档注释

---
+++ chapter-01/CommentTest.java
@@ -0,0 +1,18 @@
+/**
+ * 我是文档注释: 这是我的第一个Java程序, 很开心!
+ * @author ZhangSan
+ * @version 1.0
+ */
+
+public class CommentTest {
+  /**
+   * 这是main()方法, 是Java程序的入口
+   */
+
+  /*
+   * 我是多行注释, 我不会被javadoc这个工具所读取
+   */
+  public static void main(String[] args) {
+    System.out.println("我是多行注释");
+  }
+}
